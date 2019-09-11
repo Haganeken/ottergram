8372cbc978ba48f8522009c3758a384b3ed6ed21
Haganeken <ktat73@csu.fullerton.edu>
2019-09-10 21:07:47 -0700

    Chapter5/6/7update
    5/6/7working,
    Linter flagging console command
    Need to add next and previous buttons

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+ "env": {
+ "browser": true
+ },
+ "extends": "eslint:recommended", "rules": {
+ "indent": [
+ "error",
+ 2
+],
+"linebreak-style": [
+ "error",
+ "windows"
+],
+"quotes": [
+ "error",
+ "double"
+],
+"semi": [
+ "error",
+ "always"
+]
+}
+};
