140120db30d17058684c5019380f85fea374aead
Haganeken <ktat73@csu.fullerton.edu>
2019-09-17 18:28:03 -0700

    Beautify+buttons
    Beautified files, added buttons
    Some functionality is there, but not correct.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,5 @@ module.exports = {
  "error",
  "always"
 ]
-}
+ }
 };
--- scripts/main.js
+++ scripts/main.js
@@ -81,4 +81,41 @@ function initializeEvents() {
   addKeyPressHandler();
 }
 
+//WIP button action w3schools
+/*var slideIndex = 1;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n)
+{
+  var i;
+  var slides = document.getElementsByClassName("thumbnail-item");
+  var dots = document.getElementsByClassName("dot");
+  if ( n > slides.length)
+  {
+    slideIndex=1;
+  }
+  if( n < 1)
+  {
+    slideIndex = slides.length;
+  }
+  for ( i = 0 ; i < slides.length; i++)
+  {
+    slides[i].style.display = "none";
+  }
+  for ( i = 0; i < dots.length; i++)
+  {
+    dots[i].className= dots[i].classNAme.replace("active","");
+  }
+  slides[slideIndex-1].style.display="block";
+  dots[slideIndex-1].className += "active";
+}*/
+
+
+
 initializeEvents();
